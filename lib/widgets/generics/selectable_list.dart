4cb82a39d88fb1088f66c8e4694eb8e28cddf843
Michał Hajduk <hajduk.michal1998@gmail.com>
2023-01-03 19:19:15 +0100

    Add filterBuilder to SelectableList

--- lib/widgets/generics/selectable_list.dart
+++ lib/widgets/generics/selectable_list.dart
@@ -4,7 +4,13 @@ import '../../models/bought_product.dart';
 import '../pages/widgets/fab/action_button.dart';
 import 'models/selectable_item_.dart';
 abstract class SelectableList extends StatefulWidget {
-  SelectableList({Key? key, required List<BoughtProduct> data, required this.listKey, required this.onBulkActions, required this.noBulkActions, this.scrollController}) : _data = data, super(key: key);
+  SelectableList({
+    Key? key, 
+    required List<BoughtProduct> data, 
+    required this.listKey, 
+    required this.onBulkActions, 
+    required this.noBulkActions, 
+    this.scrollController}) : _data = data, super(key: key);
 
   final GlobalKey<AnimatedListState> listKey;
   final ScrollController? scrollController;
@@ -19,6 +25,8 @@ abstract class SelectableList extends StatefulWidget {
   State<SelectableList> createState() => SelectableListState();
 
   Widget buildChildren(SelectableItem<BoughtProduct> product, Animation<double> animation);
+
+  Widget buildFilter(BuildContext context, Function (List<SelectableItem<BoughtProduct>>) setStateOverride);
 }
 
 class SelectableListState extends State<SelectableList> {
@@ -40,13 +48,7 @@ class SelectableListState extends State<SelectableList> {
     return Scaffold(
       body: Column(
         children: [
-          TextField(
-            style: Theme.of(context).textTheme.labelSmall,
-            onChanged: (value) => _runFilter(value),
-            decoration: const InputDecoration(
-              labelText: 'Wyszukaj produkt', 
-              suffixIcon: Icon(Icons.search)),
-          ),
+          widget.buildFilter(context, setFilterState),
           const SizedBox(
             height: 25,
           ),
@@ -100,17 +102,8 @@ class SelectableListState extends State<SelectableList> {
     );
   }
 
-  void _runFilter(String enteredKeyword) {
-    // TODO - można dodać tą samą animacje co przy usuwaniu wiele elementów (troche dziwne to będzie - taki dodatkowy quest)
-    List<SelectableItem<BoughtProduct>> results = [];
-    if (enteredKeyword.isEmpty) {
-      results = widget.selectableItems;
-    } else {
-      results = widget.selectableItems
-        .where((item) => item.data.name!.toLowerCase().contains(enteredKeyword.toLowerCase()))
-        .toList();
-    }
-    searchableBoughtProducts = results;
+  void setFilterState(List<SelectableItem<BoughtProduct>> products) {
+    searchableBoughtProducts = products; 
     setState(() { });
   }
 }
