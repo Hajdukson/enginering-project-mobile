fae135b5c75dcc414665806c4d0976ac7375da5c
Michał Hajduk <hajduk.michal1998@gmail.com>
2023-01-16 15:50:13 +0100

    Convert Chart to StatefulWidget, add tooltip details, modify tooltip

--- lib/widgets/pages/widgets/chart.dart
+++ lib/widgets/pages/widgets/chart.dart
@@ -2,9 +2,10 @@
 import 'package:date_util/date_util.dart';
 import 'package:fl_chart/fl_chart.dart';
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 import 'package:money_manager_mobile/models/bought_product.dart';
 
-class Chart extends StatelessWidget {
+class Chart extends StatefulWidget {
   Chart(this.boughtProducts, {Key? key})  : super(key: key) 
   {
     boughtProducts.sort((first, next) => first.boughtDate!.compareTo(next.boughtDate!));
@@ -12,8 +13,22 @@ class Chart extends StatelessWidget {
 
   final List<BoughtProduct> boughtProducts;
 
+  @override
+  State<Chart> createState() => _ChartState();
+}
+
+class _ChartState extends State<Chart> {
+  late int showingTooltipSpot;
+
+  @override
+  void initState(){
+    showingTooltipSpot = -1;
+    super.initState();
+  }
+
+
   List<double> get maxDimValues {
-    var prcies = boughtProducts.map((e) => e.price).toList();
+    var prcies = widget.boughtProducts.map((e) => e.price).toList();
     prcies.sort();
     double maxValue = 0;
     double minValue = 0;
@@ -32,16 +47,16 @@ class Chart extends StatelessWidget {
 
     return [minValue, maxValue];
   }
-  
+
   Map<int, int> get daysInMonths {
     Map<int, int> result = {};
     var dateUtility = DateUtil();
-    for(int i = 0; i < boughtProducts.length; i++) {
-      var month = boughtProducts[i].boughtDate!.month;
-      var year = boughtProducts[i].boughtDate!.year;
+    for(int i = 0; i < widget.boughtProducts.length; i++) {
+      var month = widget.boughtProducts[i].boughtDate!.month;
+      var year = widget.boughtProducts[i].boughtDate!.year;
       var days = dateUtility.daysInMonth(month, year);
       result[month] = days;
-      if(i == boughtProducts.length - 1) {
+      if(i == widget.boughtProducts.length - 1) {
         days = dateUtility.daysInMonth(month + 1, month == 12 ? year + 1 : year);
         result[month + 1] = days;
       }
@@ -56,22 +71,22 @@ class Chart extends StatelessWidget {
     double monthIndex = 0;
     double monthFactor = 0;
     
-    for (var product in boughtProducts) {
-      if(product.boughtDate!.isAfter(boughtProducts.first.boughtDate!)) {
+    for (var product in widget.boughtProducts) {
+      if(product.boughtDate!.isAfter(widget.boughtProducts.first.boughtDate!)) {
         int initial = 0;
-        int endOfloop = 0;
-        if(boughtProducts.first.boughtDate!.month > product.boughtDate!.month) {
+        int closing = 0;
+        if(widget.boughtProducts.first.boughtDate!.month > product.boughtDate!.month) {
           monthIndex++;
           initial = 1;
-          endOfloop = product.boughtDate!.month;
+          closing = product.boughtDate!.month;
         } else {
-          initial = boughtProducts.first.boughtDate!.month;
-          endOfloop = product.boughtDate!.month;
+          initial = widget.boughtProducts.first.boughtDate!.month;
+          closing = product.boughtDate!.month;
         }
 
-        for(int i = initial; i < endOfloop; i++) {
+        for(int i = initial; i < closing; i++) {
           monthIndex++;
-          if(i == endOfloop - 1) {
+          if(i == closing - 1) {
             monthFactor = 1 / dateUtility.daysInMonth(i, product.boughtDate!.year);
           }
         }
@@ -97,50 +112,112 @@ class Chart extends StatelessWidget {
           maxY: maxDimValues[1],
           minY: maxDimValues[0],
           gridData: FlGridData(show: false),
-          lineBarsData: [
-            LineChartBarData(
-              isCurved: true,
-              curveSmoothness: 0,
-              color: const Color(0x444af699),
-              barWidth: 4,
-              isStrokeCapRound: true,
-              dotData: FlDotData(show: false),
-              belowBarData: BarAreaData(show: false),
-              spots: [
-                ...drawChart()
-              ],
-            )
-          ],
-          titlesData: FlTitlesData(
-            bottomTitles: AxisTitles(
-              sideTitles: SideTitles(
-              showTitles: true,
-              reservedSize: 32,
-              interval: 1,
-              getTitlesWidget: bottomTitleWidgets,
-              ),),
-            rightTitles: AxisTitles(
-              sideTitles: SideTitles(showTitles: false),
-            ),
-            topTitles: AxisTitles(
-              sideTitles: SideTitles(showTitles: false),
-            ),
-            leftTitles: AxisTitles(
-              sideTitles: leftTitles(),
-            ),),
-          borderData: FlBorderData(
-            show: true,
-            border: const Border(
-              bottom: BorderSide(color: Color(0xff4e4965), width: 4),
-              left: BorderSide(color: Colors.transparent),
-              right: BorderSide(color: Colors.transparent),
-              top: BorderSide(color: Colors.transparent),
-            ),
-          )
+          showingTooltipIndicators: showingTooltipSpot != -1 ? [ShowingTooltipIndicators([
+            LineBarSpot(lineChartBarData[0], showingTooltipSpot, lineChartBarData[0].spots[showingTooltipSpot])
+          ])] : [],
+          lineTouchData: lineTouchData,
+          lineBarsData: lineChartBarData,
+          titlesData: tilesData,
+          borderData: botderData
         )
       ),
     );
   }
+
+  FlBorderData get botderData => FlBorderData(
+    show: true,
+    border: const Border(
+      bottom: BorderSide(color: Color(0xff4e4965), width: 4),
+      left: BorderSide(color: Colors.transparent),
+      right: BorderSide(color: Colors.transparent),
+      top: BorderSide(color: Colors.transparent),
+    ),
+  );
+
+  FlTitlesData get tilesData => FlTitlesData(
+    bottomTitles: AxisTitles(
+      sideTitles: SideTitles(
+      showTitles: true,
+      reservedSize: 32,
+      interval: 1,
+      getTitlesWidget: bottomTitleWidgets,
+      ),),
+    rightTitles: AxisTitles(
+      sideTitles: SideTitles(showTitles: false),
+    ),
+    topTitles: AxisTitles(
+      sideTitles: SideTitles(showTitles: false),
+    ),
+    leftTitles: AxisTitles(
+      sideTitles: leftTitles(),
+    ),
+  );
+
+  LineTouchData get lineTouchData => LineTouchData(
+    touchTooltipData: LineTouchTooltipData(
+      tooltipBgColor: Colors.blueGrey,
+      tooltipRoundedRadius: 10.0,
+      fitInsideHorizontally: true,
+      tooltipMargin: 10,
+      getTooltipItems: (touchedSpots) {
+        return touchedSpots.map(
+          (LineBarSpot touchedSpot) {
+            const textStyle = TextStyle(
+              fontSize: 13,
+              fontWeight: FontWeight.w700,
+              color: Colors.white,
+              );
+             return LineTooltipItem(
+                "",
+                textStyle,
+                children: [
+                  TextSpan(text: "${widget.boughtProducts[touchedSpot.spotIndex].price!.toStringAsFixed(2)} PLN\n"),
+                  TextSpan(text: DateFormat("MMMMd", "pl").format(widget.boughtProducts[touchedSpot.spotIndex].boughtDate!))] 
+              );
+            },
+          ).toList();
+        },
+      ),
+      handleBuiltInTouches: false,
+      touchCallback: (evnet, respons) {
+        if(respons?.lineBarSpots != null && evnet is FlTapUpEvent) {
+          setState(() {
+            final spotIndex = respons?.lineBarSpots?[0].spotIndex ?? -1;
+            if(spotIndex == showingTooltipSpot) {
+              showingTooltipSpot = -1;
+            } 
+            else {
+              showingTooltipSpot = spotIndex;
+            }
+          });
+        }
+      }
+
+    );
+
+  List<LineChartBarData> get lineChartBarData => [
+    LineChartBarData(
+      isCurved: true,
+      curveSmoothness: 0,
+      color: const Color(0x444af699),
+      barWidth: 4,
+      isStrokeCapRound: true,
+      dotData: FlDotData(
+        show: true,
+        checkToShowDot: (spot, barData) {
+          if(showingTooltipSpot != -1) {
+            if(barData.spots[showingTooltipSpot].x == spot.x && barData.spots[showingTooltipSpot].y == spot.y) {
+              return true;
+            }
+          }     
+          return false;
+        },),
+      belowBarData: BarAreaData(show: false),
+      spots: [
+        ...drawChart()
+      ],
+    )
+  ];
 
   Widget bottomTitleWidgets(double value, TitleMeta meta) {
     const style = TextStyle(
