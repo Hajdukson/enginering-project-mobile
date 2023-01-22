4f124fa08ab88d0d948a54607d724231b7d84515
Michał Hajduk <hajduk.michal1998@gmail.com>
2023-01-22 16:32:01 +0100

    getProductsFromLast2Month() fix

--- lib/extenstions/last_2_moth_products.dart
+++ lib/extenstions/last_2_moth_products.dart
@@ -1,13 +1,14 @@
 import 'package:money_manager_mobile/models/bought_product.dart';
 
 extension Last2MonthProducts on List<BoughtProduct> {
-  List<BoughtProduct> getProductFromLast2Month(int month, int year) {
+  List<BoughtProduct> getProductsFromLast2Month(int month, int year) {
     month = month + 1;
     // Just makeing sure, that list is sorted
     sort((first, next) => first.boughtDate!.compareTo(next.boughtDate!));
     var productsUpToChoosenMonth = where((product) => 
       product.boughtDate!.isBefore(DateTime(year, month)) && 
-      product.boughtDate!.isAfter(DateTime(year, month - 2)));
+      product.boughtDate!.isAfter(DateTime(year, month - 2))
+      || product.boughtDate!.isAtSameMomentAs(DateTime(year, month - 2, 1)));
     return productsUpToChoosenMonth.toList();
   }  
 }
