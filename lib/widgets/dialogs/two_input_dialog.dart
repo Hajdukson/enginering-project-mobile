78d1d8ccc5dbfa7003a158a0f076a84babece957
Hajdukson <hajduk.michal1998@gmail.com>
2023-01-26 22:15:15 +0100

    Two input dialog v3

--- lib/widgets/dialogs/two_input_dialog.dart
+++ lib/widgets/dialogs/two_input_dialog.dart
@@ -3,26 +3,16 @@ import 'package:flutter/material.dart';
 class TwoInputDialog extends StatelessWidget {
   TwoInputDialog({
     Key? key,
-    this.firstLable, 
-    required this.firstInput, 
-    required this.firstInputMessage, 
-    this.secondLable,
-    required this.secoundInput,
-    required this.secoundInputMessage,
-    required this.submitButtonText,
-    required this.submitHandler,
     required this.formKey,
+    required this.submitHandler,
+    required this.firstInput,
+    required this.secondInput
     }) : super(key: key);
-
-  final TextEditingController firstInput;
-  final String firstInputMessage;
-  final TextEditingController secoundInput;
-  final String secoundInputMessage;
-  final String submitButtonText;
-  final VoidCallback submitHandler;
   final GlobalKey<FormState> formKey;
-  String? firstLable;
-  String? secondLable;
+  final VoidCallback submitHandler;
+  final Widget firstInput;
+  final Widget secondInput;
+
   
   @override
   Widget build(BuildContext context) {
@@ -33,40 +23,19 @@ class TwoInputDialog extends StatelessWidget {
           margin: const EdgeInsets.all(10),
           height: 250,
           child: Column(children: [
-            TextFormField(
-              decoration: InputDecoration(
-                label: Text(firstLable ?? ""),
-              ),  
-              controller: firstInput,
-              validator: (value) {
-                if(value == null || value.isEmpty) {
-                  return firstInputMessage;
-                }
-              },
-            ),
+            firstInput,
             const SizedBox(height: 20,),
-            TextFormField(
-              decoration: InputDecoration(
-                label: Text(secondLable ?? "")
-              ),
-              controller: secoundInput,
-              validator: (value) {
-                if(double.tryParse(value!) == null || value.isEmpty) {
-                  return secoundInputMessage;
-                }
-              },
-            ),
+            secondInput,
             const SizedBox(height: 20,),
             Expanded(
               child: Align(
                 alignment: Alignment.bottomCenter,
                 child: ElevatedButton(onPressed: () => submitHandler.call(),  
-                  child: Text(submitButtonText)),
+                  child: const Text("Zatwierdź")),
               ),
             )
           ]),
         ),
-        
       ),
     );
   }
