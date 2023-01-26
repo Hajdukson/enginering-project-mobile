71fd0b3384b823dec986b55a4229aa8458844c1e
Hajdukson <hajduk.michal1998@gmail.com>
2023-01-26 21:43:04 +0100

    Two imput dialog design update

--- lib/widgets/dialogs/two_input_dialog.dart
+++ lib/widgets/dialogs/two_input_dialog.dart
@@ -1,16 +1,19 @@
 import 'package:flutter/material.dart';
 
 class TwoInputDialog extends StatelessWidget {
-  const TwoInputDialog({
-    Key? key, 
+  TwoInputDialog({
+    Key? key,
+    this.firstLable, 
     required this.firstInput, 
     required this.firstInputMessage, 
+    this.secondLable,
     required this.secoundInput,
     required this.secoundInputMessage,
     required this.submitButtonText,
     required this.submitHandler,
     required this.formKey,
     }) : super(key: key);
+
   final TextEditingController firstInput;
   final String firstInputMessage;
   final TextEditingController secoundInput;
@@ -18,6 +21,8 @@ class TwoInputDialog extends StatelessWidget {
   final String submitButtonText;
   final VoidCallback submitHandler;
   final GlobalKey<FormState> formKey;
+  String? firstLable;
+  String? secondLable;
   
   @override
   Widget build(BuildContext context) {
@@ -26,9 +31,12 @@ class TwoInputDialog extends StatelessWidget {
         key: formKey,
         child: Container(
           margin: const EdgeInsets.all(10),
-          height: 200,
+          height: 250,
           child: Column(children: [
-            TextFormField(  
+            TextFormField(
+              decoration: InputDecoration(
+                label: Text(firstLable ?? ""),
+              ),  
               controller: firstInput,
               validator: (value) {
                 if(value == null || value.isEmpty) {
@@ -36,7 +44,11 @@ class TwoInputDialog extends StatelessWidget {
                 }
               },
             ),
+            const SizedBox(height: 20,),
             TextFormField(
+              decoration: InputDecoration(
+                label: Text(secondLable ?? "")
+              ),
               controller: secoundInput,
               validator: (value) {
                 if(double.tryParse(value!) == null || value.isEmpty) {
@@ -45,8 +57,13 @@ class TwoInputDialog extends StatelessWidget {
               },
             ),
             const SizedBox(height: 20,),
-            ElevatedButton(onPressed: () => submitHandler.call(),  
-              child: Text(submitButtonText))
+            Expanded(
+              child: Align(
+                alignment: Alignment.bottomCenter,
+                child: ElevatedButton(onPressed: () => submitHandler.call(),  
+                  child: Text(submitButtonText)),
+              ),
+            )
           ]),
         ),
         
