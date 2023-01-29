ccfcf2153164efe8eb441c03ef7e5309826ce848
Hajdukson <hajduk.michal1998@gmail.com>
2023-01-29 12:50:22 +0100

    Summary page rout fix

--- lib/views/product_details_view.dart
+++ lib/views/product_details_view.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:money_manager_mobile/api_calls/bought_products._api.dart';
 import 'package:money_manager_mobile/generics/selectable_list.dart';
+import 'package:money_manager_mobile/menu/menu.dart';
 import 'package:money_manager_mobile/models/bought_product.dart';
 import 'package:money_manager_mobile/models/product_summary.dart';
 import 'package:money_manager_mobile/views/details_product_list_tab_view.dart';
@@ -38,59 +39,67 @@ class _ProductDetailsViewState extends State<ProductDetailsView> {
   Widget build(BuildContext context) {
     return DefaultTabController(
       length: 2,
-      child: Scaffold(
-        resizeToAvoidBottomInset: false,
-        appBar: AppBar(
-          title: const Text("Produkt"),
-          ),
-        body: FutureBuilder<List<BoughtProduct>>(
-          future: boughtProducts,
-          builder: ((context, snapshot) {
-            if(snapshot.connectionState == ConnectionState.done) {
-              if(snapshot.hasData) {
-                return Column(
-                  children: [
-                    Padding(
-                      padding: const EdgeInsets.fromLTRB(8, 15, 8, 0),
-                      child: TabBar(
-                        indicator: BoxDecoration(
-                          borderRadius: BorderRadius.circular(
-                            25.0,
-                          ),
-                          color: Theme.of(context).primaryColor,
-                        ),
-                        tabs: const [
-                          Tab(text: "Szczegóły",),
-                          Tab(text: "Lista"),
-                        ],
-                      ),
-                    ),
-                    Expanded(
-                      child: TabBarView(
-                        children: [
-                          SingleChildScrollView(
-                            child: Column(
-                              children: [
-                                Padding(
-                                  padding: const EdgeInsets.fromLTRB(20, 20, 20, 20),
-                                  child: DetailsChartTile(productSummary: widget.productSummary, boughtProducts: snapshot.data!)),
-                              ],
+      child: WillPopScope(
+        onWillPop: () async {
+          Navigator.of(context).push(
+            MaterialPageRoute(builder: (context) => const Menu())
+          );
+          return true;
+        },
+        child: Scaffold(
+          resizeToAvoidBottomInset: false,
+          appBar: AppBar(
+            title: const Text("Produkt"),
+            ),
+          body: FutureBuilder<List<BoughtProduct>>(
+            future: boughtProducts,
+            builder: ((context, snapshot) {
+              if(snapshot.connectionState == ConnectionState.done) {
+                if(snapshot.hasData) {
+                  return Column(
+                    children: [
+                      Padding(
+                        padding: const EdgeInsets.fromLTRB(8, 15, 8, 0),
+                        child: TabBar(
+                          indicator: BoxDecoration(
+                            borderRadius: BorderRadius.circular(
+                              25.0,
                             ),
+                            color: Theme.of(context).primaryColor,
                           ),
-                          Padding(
-                            padding: const EdgeInsets.fromLTRB(10, 0, 10, 0),
-                            child: DetailsProductListTabView(
-                              key: detailsProductListKey,
-                              productName: widget.productSummary.productName!,
-                              boughtProducts: snapshot.data!,))
-                        ],),
-                    ),
-                  ],
-                );
+                          tabs: const [
+                            Tab(text: "Szczegóły",),
+                            Tab(text: "Lista"),
+                          ],
+                        ),
+                      ),
+                      Expanded(
+                        child: TabBarView(
+                          children: [
+                            SingleChildScrollView(
+                              child: Column(
+                                children: [
+                                  Padding(
+                                    padding: const EdgeInsets.fromLTRB(20, 20, 20, 20),
+                                    child: DetailsChartTile(productSummary: widget.productSummary, boughtProducts: snapshot.data!)),
+                                ],
+                              ),
+                            ),
+                            Padding(
+                              padding: const EdgeInsets.fromLTRB(10, 0, 10, 0),
+                              child: DetailsProductListTabView(
+                                key: detailsProductListKey,
+                                productName: widget.productSummary.productName!,
+                                boughtProducts: snapshot.data!,))
+                          ],),
+                      ),
+                    ],
+                  );
+                }
               }
-            }
-            return const Center(child: CircularProgressIndicator());
-        } )),
+              return const Center(child: CircularProgressIndicator());
+          } )),
+        ),
       ),
     );
   }
